0667797f1f64606f8181c3e29499644cd6774547
MisterTakaashi <soapmctravich@gmail.com>
2022-03-03 22:40:44 +0100

    add the public ip to get drone ip in ping

--- citadel-drone/src/controllers/ping.ts
+++ citadel-drone/src/controllers/ping.ts
@@ -1,10 +1,11 @@
 import { Context } from 'koa';
+import * as publicIp from 'public-ip';
 import { commonControllers } from 'citadel-lib';
 
 class PingController extends commonControllers.ApplicationController {
   // GET /ping
   async index(ctx: Context) {
-    this.renderSuccess(ctx, { ping: 'pong' });
+    this.renderSuccess(ctx, { ping: 'pong', ip: await publicIp.v4() });
   }
 }
 
