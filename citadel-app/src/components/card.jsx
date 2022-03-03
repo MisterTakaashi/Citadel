c194693284ee0bc1a8ffdc9242e849d6c78ea060
MisterTakaashi <soapmctravich@gmail.com>
2022-03-03 21:53:02 +0100

    add the card component

---
+++ citadel-app/src/components/card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Card({ title, children, className }) {
+  return (
+    <div className={`dark:bg-gray-800 dark:text-white p-5 rounded ${className}`}>
+      <p className='text-2xl px-3'>{title}</p>
+      <div className='h-px bg-gray-600 mt-5 mb-5' />
+      <div className='px-3'>{children}</div>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+Card.defaultProps = {
+  className: '',
+  children: <div />,
+};
+
+export default Card;
