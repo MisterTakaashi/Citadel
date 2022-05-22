a6453fcde58532ca0fc25aa52791e90e985b1731
MisterTakaashi <soapmctravich@gmail.com>
2022-05-22 23:07:03 +0200

    add the lib config for minecraft

--- citadel-lib/src/games.ts
+++ citadel-lib/src/games.ts
@@ -4,6 +4,7 @@ enum Providers {
 
 const GAMES = [
   {name: "Garry's Mod", appId: 4000, shortcut: 'gmod', slug: 'gmod-4000', providers: [Providers.Docker]},
+  {name: "Minecraft", appId: 0, shortcut: 'minecraft', slug: 'minecraft-0', providers: [Providers.Docker]},
 ];
 
 export default GAMES;
