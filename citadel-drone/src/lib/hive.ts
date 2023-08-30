b7fc92db60745aa0c7e42d8ec93bf7ee46a082f6
MisterTakaashi <soapmctravich@gmail.com>
2023-08-30 18:12:03 +0200

    remove axios from drone

--- citadel-drone/src/lib/hive.ts
+++ citadel-drone/src/lib/hive.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import makeLogger from './logger';
 import { IJob } from '@citadelnest/lib';
 import dispatcher from '../dispatcher';
@@ -7,28 +6,39 @@ const logger = makeLogger(module);
 
 let HOST: string, TOKEN: string;
 
-const queryHive = async <T>(endpoint: string, method: 'POST' | 'PUT' | 'GET' | 'PATCH' | 'DELETE', data?: unknown) => {
-  return axios<T>({
-    baseURL: HOST,
-    headers: { 'X-Api-Key': TOKEN },
-    url: endpoint,
+const queryHive = async <T>(
+  endpoint: string,
+  method: 'POST' | 'PUT' | 'GET' | 'PATCH' | 'DELETE',
+  data?: unknown
+): Promise<T> => {
+  const response = await fetch(new URL(endpoint, HOST), {
+    headers: { 'content-type': 'application/json;charset=UTF-8', 'X-Api-Key': TOKEN },
     method,
-    data,
+    body: JSON.stringify(data),
   });
+
+  if (response.ok) return response.json();
+
+  if (response.status < 500) throw new Error((await response.json()).message);
+
+  throw new Error('Unhandled error from hive');
 };
 
 const connectToHive = async (host: string, token: string) => {
   try {
-    await axios.post(`${host}/drones/register`, {}, { headers: { 'X-Api-Key': token } });
     HOST = host;
     TOKEN = token;
+    await queryHive('/drones/register', 'POST', {});
   } catch (e) {
-    throw new Error(`Cannot connect to the hive (${e.response.data.message})`);
+    throw new Error(`Cannot connect to the hive (${e.cause ?? e.message})`);
   }
 };
 
 const pollNextJob = async () => {
-  const { status, data: apiResponse } = await queryHive<{ data: { job: IJob } }>('/jobs', 'GET');
+  const { status, data: apiResponse } = await queryHive<{ status: number; data: { data: { job: IJob } } }>(
+    '/jobs',
+    'GET'
+  );
 
   if (status === 201) return;
   if (apiResponse.data?.job === undefined || apiResponse.data.job === null) return;
