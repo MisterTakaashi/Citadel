d99bd8476fcf8b46b0329a604f2ca700b0a59b2e
Paul Derain <soapmctravich@gmail.com>
2024-07-31 15:29:50 +0200

    add nix configuration for drone

---
+++ citadel-drone/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer openssl pkg-config ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      }
+    );
+}
